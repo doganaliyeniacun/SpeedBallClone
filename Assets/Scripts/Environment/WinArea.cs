3ab59c965efdb0deb4c7d8c06f72c44979d7951f
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2023-04-06 03:31:58 +0300

    fixed movement

--- Assets/Scripts/Environment/WinArea.cs
+++ Assets/Scripts/Environment/WinArea.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class WinArea : MonoBehaviour
 {
-    
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(GameplayManager.playerName))
         {
-            GameplayManager.instance.GameOver();
+            GameplayManager.instance.YouWin();
         }
     }
 }
--- Assets/Scripts/Player/BallMovement.cs
+++ Assets/Scripts/Player/BallMovement.cs
@@ -1,23 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class BallMovement : MonoBehaviour
 {
-    [Header("Components")]
+    [Header("[Components]")]
     [SerializeField] private Rigidbody rb;
 
-    [Header("Setting")]
+    [Header("[Setting]")]
     public float moveSpeed = 10f;
     public float maxX = 5f;
     private bool turn = false;
 
 
-    void Start()
-    {
-        rb.velocity = transform.forward * moveSpeed;
-    }
-
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (turn)
         {
@@ -29,15 +25,21 @@ public class BallMovement : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        rb.velocity += new Vector3(0f, 0f, 1f) * moveSpeed * Time.fixedDeltaTime;
+    }
+
     private void OnClick()
     {
         turn = !turn;
     }
 
-    void Move(float direction)
+    private void Move(float direction)
     {
         float targetPos = Mathf.Clamp(direction, -maxX, maxX);
         Vector3 movement = new Vector3(targetPos, transform.position.y, transform.position.z);
         rb.MovePosition(movement);
     }
+
 }
